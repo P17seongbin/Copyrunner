311f1af82a94ad25d8c78186ed5aec3a440ae9a6
P17Seongbin <sungbin0515@postech.ac.kr>
2018-07-19 16:00:07 +0900

    Unit Object 중간저장

--- Terraforming/Assets/Scripts/Main/Unit.cs
+++ Terraforming/Assets/Scripts/Main/Unit.cs
@@ -9,6 +9,7 @@ public class Unit : MonoBehaviour
     public float Speed; //각 Unit의 고유 속도입니다, 단위는 Unity Meter입니다.Unity 편집기에서 각 Unit별로 별도의 값을 지정한 뒤 Prefab화 합니다.
     public float Summon_Time; //각 Unit의 고유 소환시간입니다. 단위는 초입니다. Unity 편집기에서 각 Unit별로 별도의 값을 지정한 뒤 Prefab화 합니다.
     public float Attack_Speed; //각 Unit이 공격을 1회 하는데 걸리는 시간을 의미합니다.Unity 편집기에서 각 Unit별로 별도의 값을 지정한 뒤 Prefab화 합니다.
+    public BoxCollider2D Hitbox, Attack_Range;//Unit의 피격 Hitbox와 공격범위 Hitbox입니다. Editor에서 할당받습니다.
 
     private Vector3 Cur_Env; //매 Frame마다 GameManager에게 현재 환경 변수를 받아오는 것을 저장할 임시 변수입니다.
     private bool Is_Moveable; //현재 Unit.cs가 붙어 있는 Object가 움직일 수 있는지를 나타냅니다. 기본값은 True이며, 앞에 공격할 수 있는 다른 Object가 있거나 health가 0보다 작아지면 False로 변경됩니다.
@@ -17,8 +18,10 @@ public class Unit : MonoBehaviour
     private GameManager GM_Script; //GameManager Object에 붙어있는 GamemManager.cs를 나타냅니다.
     private int ID; //이 Unit의 번호를 나타냅니다. (할당된 슬롯 순서)
 
+    
     void Start()
     {
+        GM_Script = GameObject.Find("GameManager").GetComponent<GameManager>();
         Cur_Env = new Vector3 (0, 0, 0);
         Is_Moveable = true;
        
@@ -35,7 +38,7 @@ public class Unit : MonoBehaviour
         {
             Is_Moveable = false;
         }
-        else if()//(공격 가능한 Object가 사정거리 내에 있으면)
+        else if(true)//(공격 가능한 Object가 사정거리 내에 있으면)
         {
             Is_Moveable = false;
         }
@@ -76,9 +79,11 @@ public class Unit : MonoBehaviour
     {
         Cur_Env = GM_Script.Get_RGBValue();
     }
+    /*
     IEnumerator Dead_Anim()
     {
 
     }
+    */
     
 }
