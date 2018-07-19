966ed3cce5307fe57a9625bc65ba6f37b04d0618
P17Seongbin <sungbin0515@postech.ac.kr>
2018-07-19 14:00:03 +0900

    Unit.cs 기초 틀 완성

---
+++ Terraforming/Assets/Scripts/Main/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Vector3 Max_Env, Min_Env;
+    private Vector3 Cur_Env;
+    private float Init_R, Init_G, Init_B;
+    public Vector3 Get_RGBValue()
+    {
+        return Cur_Env;
+    }
+    public void Set_RGBValue(Vector3 RGB)
+    {
+        //미리 설정된 최대값과 최솟값을 넘지 않도록 값을 설정한다.
+        Vector3 Limit_RGB = Limit_RGBValue(RGB);
+        //Vector3형은 x,y,z를 하나씩 바꿀 수 없기 때문에 새로 만든 RGB 값을 할당해준다.
+        Cur_Env = Limit_RGB;
+    }
+    // Use this for initialization
+    void Start () {
+        //초기값을 설정하고 제한값을 넘지 않도록 조율해줍니다.
+        Init_R = 5f;
+        Init_G = 5f;
+        Init_B = 5f;
+        Cur_Env = new Vector3(Init_R, Init_G, Init_B);
+        Cur_Env = Limit_RGBValue(Cur_Env);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private Vector3 Limit_RGBValue(Vector3 RGB)
+    {
+        float R = Mathf.Min(Mathf.Max(Min_Env.x, RGB.x), Max_Env.x);
+        float G = Mathf.Min(Mathf.Max(Min_Env.y, RGB.y), Max_Env.y);
+        float B = Mathf.Min(Mathf.Max(Min_Env.z, RGB.z), Max_Env.z);
+        return new Vector3(R, G, B);
+    }
+}
---
+++ Terraforming/Assets/Scripts/Main/Unit.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public int Max_Health, Health;// 최대 체력수치/현재 체력수치를 나타냅니다, Unity 편집기에서 각 Unit별로 고유의 값을 지정합니다.
+    public float Cost; // 이 Unit을 소환하는데 필요한 자원(에너지) 의 양입니다.Unity 편집기에서 각 Unit별로 별도의 값을 지정한 뒤 Prefab화 합니다.
+    public float Speed; //각 Unit의 고유 속도입니다, 단위는 Unity Meter입니다.Unity 편집기에서 각 Unit별로 별도의 값을 지정한 뒤 Prefab화 합니다.
+    public float Summon_Time; //각 Unit의 고유 소환시간입니다. 단위는 초입니다. Unity 편집기에서 각 Unit별로 별도의 값을 지정한 뒤 Prefab화 합니다.
+    public float Attack_Speed; //각 Unit이 공격을 1회 하는데 걸리는 시간을 의미합니다.Unity 편집기에서 각 Unit별로 별도의 값을 지정한 뒤 Prefab화 합니다.
+
+    private Vector3 Cur_Env; //매 Frame마다 GameManager에게 현재 환경 변수를 받아오는 것을 저장할 임시 변수입니다.
+    private bool Is_Moveable; //현재 Unit.cs가 붙어 있는 Object가 움직일 수 있는지를 나타냅니다. 기본값은 True이며, 앞에 공격할 수 있는 다른 Object가 있거나 health가 0보다 작아지면 False로 변경됩니다.
+    private GameObject HQ; //Unit을 소환한 HeadQuarter GameObject를 저장합니다.
+    private int ID; //이 Unit의 번호를 나타냅니다. (할당된 슬롯 순서)
+    // Use this for initialization
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Init(int Team, int _ID, GameObject HeadQuarter)
+    {
+
+    }
+    public void Hit(float damage)
+    {
+
+    }
+    public float Get_Cost()
+    {
+
+    }
+    public float Get_SummonTime()
+    {
+
+    }
+    private void Move()
+    {
+
+    }
+    private void Check_Dead()
+    {
+
+    }
+    private void Get_RGBValue()
+    {
+
+    }
+    IEnumerator Dead_Anim()
+    {
+
+    }
+}
