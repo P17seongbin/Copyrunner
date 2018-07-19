606f1e5b4be151a14e4e01fcc6e99537fec1f47c
김수빈 <ksb991022@gmail.com>
2018-07-19 15:16:34 +0900

    Frozen에서 작업

--- Terraforming/Assets/Scripts/Main/Unit.cs
+++ Terraforming/Assets/Scripts/Main/Unit.cs
@@ -12,10 +12,13 @@ public class Unit : MonoBehaviour
 
     private Vector3 Cur_Env; //매 Frame마다 GameManager에게 현재 환경 변수를 받아오는 것을 저장할 임시 변수입니다.
     private bool Is_Moveable; //현재 Unit.cs가 붙어 있는 Object가 움직일 수 있는지를 나타냅니다. 기본값은 True이며, 앞에 공격할 수 있는 다른 Object가 있거나 health가 0보다 작아지면 False로 변경됩니다.
+    private int Team; //이 Unit이 속해있는 팀을 나타냅니다. 여기서 Team은 1 또는 -1입니다.
     private GameObject HQ; //Unit을 소환한 HeadQuarter GameObject를 저장합니다.
+    private GameManager GM_Script; //GameManager Object에 붙어있는 GamemManager.cs를 나타냅니다.
     private int ID; //이 Unit의 번호를 나타냅니다. (할당된 슬롯 순서)
-    // Use this for initialization
 
+
+    // Use this for initialization
     void Start()
     {
 
@@ -27,9 +30,11 @@ public class Unit : MonoBehaviour
 
     }
 
-    public void Init(int Team, int _ID, GameObject HeadQuarter)
+    public void Init(int _Team, int _ID, GameObject HeadQuarter)
     {
-
+        Team = _Team; //팀의 정보를 Unit에게 넘겨줍니다. 여기서 Team은 1 또는 -1입니다.
+        ID = _ID; //Unit의 번호를 Unit에게 넘겨줍니다.
+        HQ = HeadQuarter; //Unit을 소환한 HeadQuarter GameObject를 Unit에게 넘겨줍니다.
     }
     public void Hit(float damage)
     {
@@ -45,7 +50,10 @@ public class Unit : MonoBehaviour
     }
     private void Move()
     {
-
+        if (Is_Moveable) //움직일 수 있는지를 판정합니다.
+        {
+            transform.position = transform.position + new Vector3(1 * Team, 0, 0) * Time.deltaTime * Speed; //Unit을 Team의 여부에 따라 다르게 움직입니다. 여기서 Team은 1 또는 -1입니다.
+        }
     }
     private void Check_Dead()
     {
@@ -53,7 +61,7 @@ public class Unit : MonoBehaviour
     }
     private void Get_RGBValue()
     {
-
+        Cur_Env = GM_Script.Get_RGBValue();
     }
     IEnumerator Dead_Anim()
     {
