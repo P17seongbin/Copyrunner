2ed32154b893072cbe34963e8db427535eceac4d
김수빈 <ksb991022@gmail.com>
2018-07-26 17:23:41 +0900

    오류

--- Terraforming/Assets/Scripts/Main/HeadQuarter.cs
+++ Terraforming/Assets/Scripts/Main/HeadQuarter.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeadQuarter : MonoBehaviour {
 
     public int Team;
     public int MAX_UNIT_VARIATION=2; //플레이어가 가질 수 있는 유닛의 총 개수를 의미합니다, Defalut값은 3입니다.
 
-    bool Is_Casting; //주문을 시전하고 있는지 여부를 나타냅니다.Default값은 False입니다.
     bool Is_Paused; //현재 게임이 일시정지 되었는지를 나타냅니다.Default값은 False입니다.
 
     [SerializeField] float Health; //HQ의 현재 체력을 나타냅니다.
@@ -15,7 +15,7 @@ public class HeadQuarter : MonoBehaviour {
 
     int[] Unit_Count; //종류별 현재 Unit의 수를 나타냅니다.Summon_Unit() 함수를 호출할 때 마다 각 종류에 해당하는 값이 1씩 증가하며, Removed() 함수가 호출될 때 마다 각 Unit 종류에 해당하는 값이 1씩 감소합니다.
     float[] Unit_Cost;
-    float[] Unit_Summon_Time; //플레이어가 선택한 Unit의 Cost와 Summon_Time을 저장합니다. Object Pooling 과정에서 저장합니다.
+   // float[] Unit_Summon_Time; //플레이어가 선택한 Unit의 Cost와 Summon_Time을 저장합니다. Object Pooling 과정에서 저장합니다.
     GameObject[] Unit_Template; //플레이어가 선택한 Unit의 Prefab을 저장합니다, Object Pooling 과정에서 이 Prefab을 활용하여 진행합니다.
     int[] Unit_Queue = new int[5]; //소환 대기열에 들어있는 Unit의 번호를 나타냅니다.
 
@@ -76,9 +76,9 @@ public class HeadQuarter : MonoBehaviour {
     }
     
     //이 함수는 건드리지 마세요
-    public void Pause_Toggle(bool Is_Pause)
+    public void Set_Pause(bool Is_Pause)
     {
-
+        Is_Paused = Is_Pause;
     }
 
     public void Finish_Game()
@@ -96,22 +96,13 @@ public class HeadQuarter : MonoBehaviour {
         if (0 <= ID && ID < MAX_UNIT_VARIATION)
             Unit_Count[ID] = Unit_Count[ID] - 1;
     }
-    public void Toggle_Is_Casting()
-    {
-        Is_Casting = !Is_Casting;
-    }
-    //이 함수는 아직 건드리지 마세요
-    private void Object_Pooling()
-    {
-
-    }
     // Use this for initialization
     void Start()
     {
 
         Unit_Count = new int[MAX_UNIT_VARIATION]; //종류별 현재 Unit의 수를 나타냅니다.Summon_Unit() 함수를 호출할 때 마다 각 종류에 해당하는 값이 1씩 증가하며, Removed() 함수가 호출될 때 마다 각 Unit 종류에 해당하는 값이 1씩 감소합니다.
         Unit_Cost = new float[MAX_UNIT_VARIATION];
-        Unit_Summon_Time = new float[MAX_UNIT_VARIATION]; //플레이어가 선택한 Unit의 Cost와 Summon_Time을 저장합니다. Object Pooling 과정에서 저장합니다.
+      //  Unit_Summon_Time = new float[MAX_UNIT_VARIATION]; //플레이어가 선택한 Unit의 Cost와 Summon_Time을 저장합니다. Object Pooling 과정에서 저장합니다.
         Unit_Template = new GameObject[MAX_UNIT_VARIATION]; //플레이어가 선택한 Unit의 Prefab을 저장합니다, Object Pooling 과정에서 이 Prefab을 활용하여 진행합니다.
 
         Unit_Template = GameObject.Find("GameManager").GetComponent<GameManager>().Get_UnitLIst(Team, MAX_UNIT_VARIATION);
@@ -127,4 +118,9 @@ public class HeadQuarter : MonoBehaviour {
     {
         Resource = Resource + Time.deltaTime;
 	}
+
+    public float Get_Resource() //Resource_display.cs 에서 이 함수를 호출한다.
+    {
+        return Resource;
+    }
 }
---
+++ Terraforming/Assets/Scripts/Main/Information_display.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Text object에 이 스크립트를 적용
+
+public class Information_display : MonoBehaviour
+{
+    public Text Information_Text;
+
+    private float Resource;
+    private float HP;
+
+    [SerializeField] private bool Is_Resource_Display; //UI가 자원을 표시할 때 이 값을 true로 설정하고 체력을 표시할 때 는 false로 설정한다. 
+    [SerializeField] private bool Is_HP_Display;      //UI가 HP을 표시할 때 이 값을 true로 설정하고 자원을 표시할 때 는 false로 설정한다. 
+    [SerializeField] private int player; //player 변수의 값에 따라 1P_HQ 또는 2P_HQ 의 Resource 또는 HP값을 참조한다. 사전에 1 또는 2의 값을 설정한다.
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Is_Resource_Display == true && Is_HP_Display == false)
+            Resource_Update();
+        else if (Is_Resource_Display == false && Is_HP_Display == true)
+            Hp_Update();
+    }
+
+    private void Resource_Update()
+    {
+        if (player == 1)
+            Resource = GameObject.Find("1P_HQ").GetComponent<HeadQuarter>().Get_Resource(); //HeadQuarter.cs 의 Get_Resource 함수에서 Resource 값을 받아온다.
+        else if (player == 2)
+            Resource = GameObject.Find("2P_HQ").GetComponent<HeadQuarter>().Get_Resource();
+
+        Information_Text.text = "Resource : " + Math.Round(Resource,2).ToString();
+    }
+
+    private void Hp_Update()
+    {
+        if (player == 1)
+            HP = GameObject.Find("1P_HQ").GetComponent<HeadQuarter>().HQ_Health(); //HeadQuarter.cs 의 HQ_Health 함수에서 HP 값을 받아온다.
+        else if (player == 2)
+            HP = GameObject.Find("2P_HQ").GetComponent<HeadQuarter>().HQ_Health();
+
+        Information_Text.text = "HP : " + HP.ToString();
+    }
+
+}
