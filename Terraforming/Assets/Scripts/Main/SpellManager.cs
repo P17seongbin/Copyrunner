3af16f4f0bffae1381effa26dea4094143a36a68
김수빈 <ksb991022@gmail.com>
2018-07-30 13:38:57 +0900

    SpellManager 추가

---
+++ Terraforming/Assets/Scripts/Main/SpellManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellManager : MonoBehaviour {
+
+    bool Is_Toggled;
+    int Spell_ID, Attack_Spell_ID;
+
+    public void Spell_Load(bool Is_AttackSpell, int Spell_ID)
+    {
+
+    }
+
+    private void Spell_UnLoad()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        Is_Toggled = false;
+        Spell_ID = 0;
+        Attack_Spell_ID = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
